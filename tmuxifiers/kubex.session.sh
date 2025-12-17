9d87a82be03e74d1e19c53c86bdefef452dbca10
Julien Semaan <jul.semaan@gmail.com>
2025-12-17 08:39:22 -0500

    Refactor kubex session setup in tmuxifiers

--- tmuxifiers/kubex.session.sh
+++ tmuxifiers/kubex.session.sh
@@ -1,29 +1,44 @@
 session_root src/
 
 if initialize_session "kubex"; then
-
-  new_window "dad"
-  run_cmd "cd automation-director"
-  split_v 1
-  run_cmd "cd automation-director"
-
+  session_root src/automation-operator
   new_window "operator"
-  run_cmd "cd automation-operator"
+  run_cmd "nvim"
   split_v 1
-  run_cmd "cd automation-operator"
+  run_cmd "devbox shell"
+  split_h 50
+  run_cmd "devbox shell"
+  select_pane 1
+  split_h 25
+  run_cmd "codex"
 
-  new_window "charts"
-  run_cmd "cd densify-dev-helm-charts"
+  session_root src/automation-director
+  new_window "dad"
+  run_cmd "nvim"
   split_v 1
-  run_cmd "cd densify-dev-helm-charts"
+  split_h 50
+  select_pane 1
+  split_h 25
+  run_cmd "codex"
 
+  session_root src/charts
+  new_window "dad"
+  run_cmd "nvim"
+  split_v 1
+  split_h 50
+  select_pane 1
+  split_h 25
+  run_cmd "codex"
+
+  session_root src/architecture
   new_window "arch"
-  run_cmd "cd architecture"
+  run_cmd "nvim"
   split_v 1
-  run_cmd "cd architecture"
+  split_h 50
+  select_pane 1
+  split_h 25
+  run_cmd "codex"
 
   select_window 1
   select_pane 1
 fi
-
-finalize_and_go_to_session
