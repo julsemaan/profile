154c22cf0c0427868c0086cbfee89701e1b08c35
Julien Semaan <jul.semaan@gmail.com>
2026-02-13 15:54:44 -0500

    feat(tmuxifiers): derive coding window name from T_WIN_ROOT and tune pane split ratios

--- tmuxifiers/coding.window.sh
+++ tmuxifiers/coding.window.sh
@@ -2,15 +2,13 @@
 # Must be called before `new_window`.
 window_root "$T_WIN_ROOT"
 
-# Create new window. If no argument is given, window name will be based on
-# layout file name.
-new_window "coding"
+# Create new window using the basename of `T_WIN_ROOT`.
+new_window "$(basename "$T_WIN_ROOT")"
 run_cmd "nvim"
-split_v 1
+split_v 25
 run_cmd "devbox shell"
 split_h 50
 run_cmd "devbox shell"
 select_pane 1
-split_h 25
+split_h 30
 run_cmd "codex-unleashed-safely-src"
-
