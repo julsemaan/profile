9779eccb26d712137dc9c9801e36974ce581e3d2
Julien Semaan <jsemaan@densify.com>
2026-03-16 10:53:42 -0400

    fix(tmux): load kubex and kpp sessions from home with updated kubex layout

--- tmuxifiers/kubex.session.sh
+++ tmuxifiers/kubex.session.sh
@@ -1,42 +1,46 @@
 session_root src/
 
+V_SPLIT=25
+TOP_H_SPLIT=30
+BOTTOM_H_SPLIT=50
+
 if initialize_session "kubex"; then
-  session_root src/automation-operator
-  new_window "operator"
+  session_root src/automation-controller
+  new_window "controller"
   run_cmd "nvim"
-  split_v 1
+  split_v $V_SPLIT
   run_cmd "devbox shell"
-  split_h 50
+  split_h $BOTTOM_H_SPLIT
   run_cmd "devbox shell"
   select_pane 1
-  split_h 25
+  split_h $TOP_H_SPLIT
   run_cmd "codex-unleashed-safely-src"
 
   session_root src/automation-director
   new_window "dad"
   run_cmd "nvim"
-  split_v 1
-  split_h 50
+  split_v $V_SPLIT
+  split_h $BOTTOM_H_SPLIT
   select_pane 1
-  split_h 25
+  split_h $TOP_H_SPLIT
   run_cmd "codex-unleashed-safely-src"
 
   session_root src/densify-dev-helm-charts
   new_window "charts"
   run_cmd "nvim"
-  split_v 1
-  split_h 50
+  split_v $V_SPLIT
+  split_h $BOTTOM_H_SPLIT
   select_pane 1
-  split_h 25
+  split_h $TOP_H_SPLIT
   run_cmd "codex-unleashed-safely-src"
 
   session_root src/architecture
   new_window "arch"
   run_cmd "nvim"
-  split_v 1
-  split_h 50
+  split_v $V_SPLIT
+  split_h $BOTTOM_H_SPLIT
   select_pane 1
-  split_h 25
+  split_h $TOP_H_SPLIT
   run_cmd "codex-unleashed-safely-src"
 
   select_window 1
