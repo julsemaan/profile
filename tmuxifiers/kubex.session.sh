45a88cd595df3e8d7e6f008250a43fb36754ceec
Julien Semaan <jsemaan@densify.com>
2026-04-15 11:13:28 -0400

    refactor(tmuxifiers): remove duplicated session/window setup in kubex.session.sh

--- tmuxifiers/kubex.session.sh
+++ tmuxifiers/kubex.session.sh
@@ -16,26 +16,8 @@ if initialize_session "kubex"; then
   split_h $TOP_H_SPLIT
   run_cmd "opencode-unleashed-safely-src"
 
-  session_root src/automation-director
-  new_window "dad"
-  run_cmd "nvim"
-  split_v $V_SPLIT
-  split_h $BOTTOM_H_SPLIT
-  select_pane 1
-  split_h $TOP_H_SPLIT
-  run_cmd "opencode-unleashed-safely-src"
-
   session_root src/densify-dev-helm-charts
   new_window "charts"
-  run_cmd "nvim"
-  split_v $V_SPLIT
-  split_h $BOTTOM_H_SPLIT
-  select_pane 1
-  split_h $TOP_H_SPLIT
-  run_cmd "opencode-unleashed-safely-src"
-
-  session_root src/architecture
-  new_window "arch"
   run_cmd "nvim"
   split_v $V_SPLIT
   split_h $BOTTOM_H_SPLIT
